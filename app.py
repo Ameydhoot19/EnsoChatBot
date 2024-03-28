4b1218714fecb2d895db1deae1f5be76d2a34252
Ameydhoot19 <122987147+Ameydhoot19@users.noreply.github.com>
2024-03-28 15:23:13 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
-from dotenv import load_dotenv
-import pickle
-from PyPDF2 import PdfReader
+# from dotenv import load_dotenv
+# import pickle
+# from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-import os
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.embeddings.openai import OpenAIEmbeddings
+# from langchain.vectorstores import FAISS
+# import os
 
 # Sidebar contents
 with st.sidebar:
@@ -22,41 +22,41 @@ with st.sidebar:
     add_vertical_space(5)
     st.write('Made with ❤️')
 
-load_dotenv()
+# load_dotenv()
 
 
-def main():
-    st.header("Chat with PDF 💬")
+# def main():
+#     st.header("Chat with PDF 💬")
 
-    # upload a PDF file
-    pdf = st.file_uploader("Upload your PDF", type='pdf')
+#     # upload a PDF file
+#     pdf = st.file_uploader("Upload your PDF", type='pdf')
 
-    # st.write(pdf)
-    if pdf is not None:
-        pdf_reader = PdfReader(pdf)
+#     # st.write(pdf)
+#     if pdf is not None:
+#         pdf_reader = PdfReader(pdf)
 
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
+#         text = ""
+#         for page in pdf_reader.pages:
+#             text += page.extract_text()
 
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-            )
-        chunks = text_splitter.split_text(text=text)
-        st.write(chunks)
+#         text_splitter = RecursiveCharacterTextSplitter(
+#             chunk_size=1000,
+#             chunk_overlap=200,
+#             length_function=len
+#             )
+#         chunks = text_splitter.split_text(text=text)
+#         st.write(chunks)
 
-        # # embeddings
-        embeddings = OpenAIEmbeddings()
-        VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-        store_name = pdf.name[:-4]
-        with open(f"{store_name}.pkl", "wb") as f:
-            pickle.dump(VectorStore, f)
+#         # # embeddings
+#         embeddings = OpenAIEmbeddings()
+#         VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+#         store_name = pdf.name[:-4]
+#         with open(f"{store_name}.pkl", "wb") as f:
+#             pickle.dump(VectorStore, f)
 
-        # st.write(chunks)        
+#         # st.write(chunks)        
 
-        # st.write(text)
+#         # st.write(text)
 
 
 if __name__ == '__main__':
